560762701a44695175c127e572a8416a5581c973
Igor Drapeza <desnigor@mail.ru>
2022-11-10 00:10:15 +0300

    added autogeneration controllers and crud for them

---
+++ autorepairProj/Controllers/MechanicsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using autorepairProj.Data;
+using autorepairProj.Models;
+
+namespace autorepairProj.Controllers
+{
+    public class MechanicsController : Controller
+    {
+        private readonly AutorepairContext _context;
+
+        public MechanicsController(AutorepairContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Mechanics
+        public async Task<IActionResult> Index()
+        {
+            var autorepairContext = _context.Mechanics.Include(m => m.Qualification);
+            return View(await autorepairContext.ToListAsync());
+        }
+
+        // GET: Mechanics/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mechanic = await _context.Mechanics
+                .Include(m => m.Qualification)
+                .FirstOrDefaultAsync(m => m.MechanicId == id);
+            if (mechanic == null)
+            {
+                return NotFound();
+            }
+
+            return View(mechanic);
+        }
+
+        // GET: Mechanics/Create
+        public IActionResult Create()
+        {
+            ViewData["QualificationType"] = new SelectList(_context.Qualifications, "QualificationId", "QualificationId");
+            return View();
+        }
+
+        // POST: Mechanics/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MechanicId,FirstName,MiddleName,LastName,QualificationType,Experience")] Mechanic mechanic)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(mechanic);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["QualificationType"] = new SelectList(_context.Qualifications, "QualificationId", "QualificationId", mechanic.QualificationType);
+            return View(mechanic);
+        }
+
+        // GET: Mechanics/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mechanic = await _context.Mechanics.FindAsync(id);
+            if (mechanic == null)
+            {
+                return NotFound();
+            }
+            ViewData["QualificationType"] = new SelectList(_context.Qualifications, "QualificationId", "QualificationId", mechanic.QualificationType);
+            return View(mechanic);
+        }
+
+        // POST: Mechanics/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MechanicId,FirstName,MiddleName,LastName,QualificationType,Experience")] Mechanic mechanic)
+        {
+            if (id != mechanic.MechanicId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mechanic);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MechanicExists(mechanic.MechanicId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["QualificationType"] = new SelectList(_context.Qualifications, "QualificationId", "QualificationId", mechanic.QualificationType);
+            return View(mechanic);
+        }
+
+        // GET: Mechanics/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mechanic = await _context.Mechanics
+                .Include(m => m.Qualification)
+                .FirstOrDefaultAsync(m => m.MechanicId == id);
+            if (mechanic == null)
+            {
+                return NotFound();
+            }
+
+            return View(mechanic);
+        }
+
+        // POST: Mechanics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var mechanic = await _context.Mechanics.FindAsync(id);
+            _context.Mechanics.Remove(mechanic);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MechanicExists(int id)
+        {
+            return _context.Mechanics.Any(e => e.MechanicId == id);
+        }
+    }
+}
---
+++ autorepairProj/Controllers/PaymentsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using autorepairProj.Data;
+using autorepairProj.Models;
+
+namespace autorepairProj.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private readonly AutorepairContext _context;
+
+        public PaymentsController(AutorepairContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Payments
+        public async Task<IActionResult> Index()
+        {
+            var autorepairContext = _context.Payments.Include(p => p.Car).Include(p => p.Mechanic);
+            return View(await autorepairContext.ToListAsync());
+        }
+
+        // GET: Payments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.Payments
+                .Include(p => p.Car)
+                .Include(p => p.Mechanic)
+                .FirstOrDefaultAsync(m => m.PaymentId == id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            return View(payment);
+        }
+
+        // GET: Payments/Create
+        public IActionResult Create()
+        {
+            ViewData["CarId"] = new SelectList(_context.Cars, "CarId", "CarId");
+            ViewData["MechanicId"] = new SelectList(_context.Mechanics, "MechanicId", "MechanicId");
+            return View();
+        }
+
+        // POST: Payments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PaymentId,CarId,Date,Cost,MechanicId,ProgressReport")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(payment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CarId"] = new SelectList(_context.Cars, "CarId", "CarId", payment.CarId);
+            ViewData["MechanicId"] = new SelectList(_context.Mechanics, "MechanicId", "MechanicId", payment.MechanicId);
+            return View(payment);
+        }
+
+        // GET: Payments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.Payments.FindAsync(id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+            ViewData["CarId"] = new SelectList(_context.Cars, "CarId", "CarId", payment.CarId);
+            ViewData["MechanicId"] = new SelectList(_context.Mechanics, "MechanicId", "MechanicId", payment.MechanicId);
+            return View(payment);
+        }
+
+        // POST: Payments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PaymentId,CarId,Date,Cost,MechanicId,ProgressReport")] Payment payment)
+        {
+            if (id != payment.PaymentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(payment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PaymentExists(payment.PaymentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CarId"] = new SelectList(_context.Cars, "CarId", "CarId", payment.CarId);
+            ViewData["MechanicId"] = new SelectList(_context.Mechanics, "MechanicId", "MechanicId", payment.MechanicId);
+            return View(payment);
+        }
+
+        // GET: Payments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.Payments
+                .Include(p => p.Car)
+                .Include(p => p.Mechanic)
+                .FirstOrDefaultAsync(m => m.PaymentId == id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            return View(payment);
+        }
+
+        // POST: Payments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var payment = await _context.Payments.FindAsync(id);
+            _context.Payments.Remove(payment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PaymentExists(int id)
+        {
+            return _context.Payments.Any(e => e.PaymentId == id);
+        }
+    }
+}
